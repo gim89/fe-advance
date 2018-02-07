a566bd6a03d72ad5f2ae2c96775adedba6264c5e
Valentyn Kyrylenko <gimalay@ukr.net>
2018-02-07 22:14:24 +0200

    reneval module-07-hw

---
+++ module-07-hw/script.js
@@ -0,0 +1,70 @@
+"use strict"
+
+const keyboard = {
+    layout: {
+        topRow: "qwertyuiop[]",
+        middleRow: "asdfghjkl;'",
+        bottomRow: "zxcvbnm,./"
+    },
+    createLayout () {
+        return `
+        <ul class="topRow">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <ul class="middleRow">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <ul class="bottomRow">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    `
+    }
+};
+
+function run() {
+    
+    keyboard.createLayout();
+    const body = document.querySelector("body");
+    body.innerHTML  = keyboard.createLayout();
+    let topRow = document.querySelectorAll('ul.topRow > li');
+    let arrTop = [...topRow];
+    arrTop.map((elem, i) => elem.textContent = keyboard.layout['topRow'][i]);
+    let middleRow = document.querySelectorAll('ul.middleRow > li');
+    let arrMiddle = [...middleRow];
+    arrMiddle.map((elem, i) => elem.textContent = keyboard.layout['middleRow'][i]);
+    let bottomRow = document.querySelectorAll('ul.bottomRow > li');
+    let arrBottom = [...bottomRow];
+    arrBottom.map((elem, i) => elem.textContent = keyboard.layout['bottomRow'][i]);
+}
+
+run();
