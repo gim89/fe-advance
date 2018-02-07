3aca9f5b8665b2c40669097cdaae9a0fc8caed6e
Valentyn Kyrylenko <gimalay@ukr.net>
2018-02-08 01:33:43 +0200

    added script

--- module-07-hw/script.js
+++ module-07-hw/script.js
@@ -7,59 +7,52 @@ const keyboard = {
         bottomRow: "zxcvbnm,./"
     },
     createLayout () {
-        return `
-        <ul class="topRow">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <ul class="middleRow">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <ul class="bottomRow">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    `
 
-    },
-    addedLayout (obj) {
-        
+        // -------------------topRow: "qwertyuiop[]"---------------------->>>
+
         const body = document.querySelector("body");
-        body.innerHTML  = obj;
+        let ulTop = document.createElement('ul');
+        ulTop.classList.add("topRow");
+
+        for (let i = 0; i < this.layout.topRow.length; i += 1){
+
+            let li = document.createElement('li');
+            ulTop.prepend(li);
+        }
+        body.prepend(ulTop);
+
         let topRow = document.querySelectorAll('ul.topRow > li');
         let arrTop = [...topRow];
         arrTop.map((elem, i) => elem.textContent = keyboard.layout['topRow'][i]);
+
+        // -------------------middleRow: "asdfghjkl;'"---------------------->>>
+
+        let ulMiddle = document.createElement('ul');
+        ulMiddle.classList.add("middleRow");
+
+        for (let i = 0; i < this.layout.middleRow.length; i += 1){
+
+            let li = document.createElement('li');
+            ulMiddle.prepend(li);
+        }
+        ulTop.after(ulMiddle);
+
         let middleRow = document.querySelectorAll('ul.middleRow > li');
         let arrMiddle = [...middleRow];
         arrMiddle.map((elem, i) => elem.textContent = keyboard.layout['middleRow'][i]);
+
+        // -------------------bottomRow: "zxcvbnm,./"---------------------->>>
+
+        let ulBottom = document.createElement('ul');
+        ulBottom.classList.add("bottomRow");
+
+        for (let i = 0; i < this.layout.bottomRow.length; i += 1){
+
+            let li = document.createElement('li');
+            ulBottom.prepend(li);
+        }
+        ulMiddle.after(ulBottom);
+
         let bottomRow = document.querySelectorAll('ul.bottomRow > li');
         let arrBottom = [...bottomRow];
         arrBottom.map((elem, i) => elem.textContent = keyboard.layout['bottomRow'][i]);
@@ -69,8 +62,79 @@ const keyboard = {
 function run() {
 
     keyboard.createLayout();
-    keyboard.addedLayout(keyboard.createLayout());
-
 }
-
 run();
+
+
+// const keyboard = {
+//     layout: {
+//         topRow: "qwertyuiop[]",
+//         middleRow: "asdfghjkl;'",
+//         bottomRow: "zxcvbnm,./"
+//     },
+//     createLayout () {
+//         return `
+//             <ul class="topRow">
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//             </ul>
+//             <ul class="middleRow">
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//             </ul>
+//             <ul class="bottomRow">
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//             </ul>
+//         `
+//     },
+//     addedLayout (obj) {
+//
+//         const body = document.querySelector("body");
+//         body.innerHTML = obj;
+//         let topRow = document.querySelectorAll('ul.topRow > li');
+//         let arrTop = [...topRow];
+//         arrTop.map((elem, i) => elem.textContent = keyboard.layout['topRow'][i]);
+//         let middleRow = document.querySelectorAll('ul.middleRow > li');
+//         let arrMiddle = [...middleRow];
+//         arrMiddle.map((elem, i) => elem.textContent = keyboard.layout['middleRow'][i]);
+//         let bottomRow = document.querySelectorAll('ul.bottomRow > li');
+//         let arrBottom = [...bottomRow];
+//         arrBottom.map((elem, i) => elem.textContent = keyboard.layout['bottomRow'][i]);
+//     }
+// };
+//
+// function run() {
+//
+//     keyboard.createLayout();
+//     keyboard.addedLayout(keyboard.createLayout());
+// }
+//
+// run();
