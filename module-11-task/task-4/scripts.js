bfe742c2d5cc11497b8e26f4f637058ae2590ac0
Valentyn Kyrylenko <gimalay@ukr.net>
2018-03-01 19:23:43 +0200

    add script

--- module-11-task/task-4/scripts.js
+++ module-11-task/task-4/scripts.js
@@ -6,34 +6,30 @@
   Результатом fetch будет ответ от сервера со статусом
   операции записи, вывести ОК или ERROR в поле result.
 */
+
 const input = document.querySelector("input");
 const postBtn = document.querySelector("#js-post");
 const result = document.querySelector(".result");
 
-postBtn.addEventListener("click", post);
-
-/*
-  @param {FormEvent} evt
-*/
 function post(evt) {
-  fetch("http://fecore.net.ua/rest/")
+    evt.preventDefault();
+    fetch("http://fecore.net.ua/rest/")
     .then(response => {
         if (response.ok) return response.json();
         throw new Error("Error fetching data");
     })
-    // .then(data => console.log(data[0].id))
     .then(data => data.map((elem, i, arr) => {
         if (input.value === data[i].name) {
             console.log(data[i].name)
             result.innerHTML = `${input.value} - ok`;
         }
-        // else {
-        //     result.innerHTML = "error";
-        // }
-
+        else {
+            result.innerHTML = "error";
+        }
     }))
     .catch(error => {
         console.error("Error: ", error);
     });
-    evt.preventDefault();
 }
+
+postBtn.addEventListener("click", post);
